0e91ce46a532fc14b696e83c04a99a026b22dc4a
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-01-21 19:19:54 +0200

    Fix script and add modify transform files

--- .github/script-yamls-to-jsons.sh
+++ .github/script-yamls-to-jsons.sh
@@ -1,8 +1,8 @@
-for dir in */ ; do
+ for dir in */ ; do
   echo "$dir"
-  # cd $dir
-  filepath = ./$dir/index.yaml; do
-    echo $filepath
-  done
-  # cd ..
+  filepath=./${dir}index.yaml
+  echo $filepath
+  cd $dir
+  python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin, Loader=yaml.FullLoader), sys.stdout, indent=4)' < index.yaml > index.json
+  cd ..
 done
