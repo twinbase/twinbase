c0a5c3393006d692832251bf317ae742fc8d0d43
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-01-28 14:00:07 +0200

    Add separate html copying script for actions

---
+++ .github/script-copy-index-html.sh
@@ -0,0 +1,7 @@
+# Remember to update commit message in actions script if you change the contents of this script
+cd docs/ 
+for dir in */ ; do
+  echo "Copying static.index.html to ${dir}index.html"
+  cp static/index.html ${dir}index.html # (Hint: trailing / is included in ${dir})
+done
+cd ..
