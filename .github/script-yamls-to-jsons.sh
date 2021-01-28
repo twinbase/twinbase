3b5800b258e3592dd2e07d30b8e1f0004d71ad30
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-01-28 13:59:09 +0200

    Enhance commenting

--- .github/script-yamls-to-jsons.sh
+++ .github/script-yamls-to-jsons.sh
@@ -1,9 +1,10 @@
+# Remember to update commit message in actions script if you change the contents of this script
 cd docs/ 
 for dir in */ ; do
-  echo "$dir"
-  filepath=./${dir}index.yaml
-  echo $filepath
+  filepath=./${dir}index.yaml # (Hint: trailing / is included in ${dir})
+  echo "Converting to JSON: $filepath"
   cd $dir
+  # Convert index.yaml to index.json with python:
   python3 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' < index.yaml > index.json
   cd ..
 done
