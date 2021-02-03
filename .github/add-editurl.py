ea3168c478f18f268177af9df383779d5e69ddf1
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-03 15:12:42 +0200

    Added file modifier script
    Script adds an editurl to all twin YAML docs

---
+++ .github/add-editurl.py
@@ -0,0 +1,24 @@
+import sys, yaml, json
+
+# ARGS: repourl, path
+
+try:
+  repo = sys.argv[1]
+  path = sys.argv[2]
+except:
+  print('Enter arguments: repourl, path')
+  exit()
+
+print('Repourl: ' + repo)
+print('Twin path:' + path)
+
+with open(r'index.yaml') as file:
+  data = yaml.load(file, Loader=yaml.FullLoader)
+
+editurl = 'https://github.com/' + repo + '/edit/main/docs/' + path + 'index.yaml'
+data['testrun']['edit'] = editurl
+
+with open(r'index2.yaml', 'w') as filew:
+  doc = yaml.dump(data, filew, default_flow_style=False, sort_keys=False)
+
+print('Added editurl: ' + editurl)
---
+++ .github/script-modify-yamls.sh
@@ -0,0 +1,7 @@
+# Remember to update commit message in actions script if you change the contents of this script
+cd docs/ 
+for dir in */ ; do
+  echo "Adding editurl to ${dir}index.yaml"
+  python3 ${GITHUB_WORKSPACE}/.github/add-editurl.py "${GITHUB_REPOSITORY}" "${dir}" # (Hint: trailing / is included in ${dir})
+done
+cd ..
