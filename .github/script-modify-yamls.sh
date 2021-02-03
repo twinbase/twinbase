90d6195adb8a62d7e3e8daa757a691999e53191c
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-03 15:32:25 +0200

    Update script-modify-yamls.sh
    Change directory before running python script

--- .github/script-modify-yamls.sh
+++ .github/script-modify-yamls.sh
@@ -1,10 +1,12 @@
 # Remember to update commit message in actions script if you change the contents of this script
 cd docs/ 
 for dir in */ ; do
+  cd $dir
   echo "Adding editurl to ${dir}index.yaml"
   echo "Current directory: $PWD"
   echo "ls:"
   ls
   python3 ${GITHUB_WORKSPACE}/.github/add-editurl.py "${GITHUB_REPOSITORY}" "${dir}" # (Hint: trailing / is included in ${dir})
+  cd ..
 done
 cd ..
