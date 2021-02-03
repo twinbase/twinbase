f8a68d92338ec7bf269734619261277e42f2c6c9
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-03 15:29:10 +0200

    Update script-modify-yamls.sh
    Print current directory and files before running python script

--- .github/script-modify-yamls.sh
+++ .github/script-modify-yamls.sh
@@ -2,6 +2,9 @@
 cd docs/ 
 for dir in */ ; do
   echo "Adding editurl to ${dir}index.yaml"
+  echo "Current directory: $PWD"
+  echo "ls:"
+  ls
   python3 ${GITHUB_WORKSPACE}/.github/add-editurl.py "${GITHUB_REPOSITORY}" "${dir}" # (Hint: trailing / is included in ${dir})
 done
 cd ..
