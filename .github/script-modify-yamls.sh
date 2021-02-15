f10b1f49c01538a882ca24d23efb83844ffdb56a
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-16 00:22:29 +0200

    Delete unneeded YAML modification scripts

--- .github/script-modify-yamls.sh
+++
@@ -1,9 +0,0 @@
-# Remember to update commit message in actions script if you change the contents of this script
-cd docs/ 
-for dir in */ ; do
-  cd $dir
-  echo "Adding editurl to ${dir}index.yaml"
-  python3 ${GITHUB_WORKSPACE}/.github/add-editurl.py "${GITHUB_REPOSITORY}" "${dir}" # (Hint: trailing / is included in ${dir})
-  cd ..
-done
-cd ..
