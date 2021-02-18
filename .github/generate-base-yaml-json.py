adf8e8a79f855027bea43a0a6eabedda565027ec
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-18 20:41:38 +0200

    Add page for adding new twin

--- .github/generate-base-yaml-json.py
+++ .github/generate-base-yaml-json.py
@@ -6,16 +6,31 @@ curdir = os.getcwd()
 
 d = {}
 
-with open('CNAME') as f:
-    lines = f.readlines()
-baseurl = 'https://' + lines[0]
-d['baseurl'] = baseurl
+repo = os.environ["GITHUB_REPOSITORY"] # for local dev, set in bash: export GITHUB_REPOSITORY=dev-user/twinbase-dev
+
+owner = repo.split('/')[0]
+print('Owner: ' + owner)
+
+repo_plain = repo.split('/')[1]
+print('Repo_plain: ' + repo_plain)
+
+try:
+    with open('CNAME') as f:
+        lines = f.readlines()
+    baseurl = 'https://' + lines[0]
+    d['baseurl'] = baseurl
+except:
+    d['baseurl'] = 'https://' + owner + '.github.io/' + repo_plain
+
+d['repo_url'] = 'https://github.com/' + repo
+
+d['new_twin_url'] = "https://github.com/" + repo + "/new/main?filename=docs/edit_this_with_backspace/index.yaml&value=Paste%20a%20DT%20document%20template%20here"
 
 print('Adding twins from folders:')
 d['twins'] = []
 i = 0
 for folder in os.listdir(curdir):
-    if os.path.isdir(folder) and folder != 'static':
+    if os.path.isdir(folder) and folder != 'static' and folder != 'new-twin':
         print('- ' + folder)
         
         with open(folder + '/index.json', 'r') as docfile:
