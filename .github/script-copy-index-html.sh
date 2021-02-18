bbdf516e4c6dc0af862e4b73f7b463bff1f0ff9d
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-18 21:32:29 +0200

    Fix faulty copying of index.html

--- .github/script-copy-index-html.sh
+++ .github/script-copy-index-html.sh
@@ -1,7 +1,10 @@
 # Remember to update commit message in actions script if you change the contents of this script
 cd docs/ 
 for dir in */ ; do
-  echo "Copying static.index.html to ${dir}index.html"
-  cp static/index.html ${dir}index.html # (Hint: trailing / is included in ${dir})
+  echo $dir
+  if ! [[ $dir == "static/" || $dir == "new-twin/" ]]; then
+    echo "Copying static.index.html to ${dir}index.html"
+    cp static/index.html ${dir}index.html # (Hint: trailing / is included in ${dir})
+  fi
 done
 cd ..
