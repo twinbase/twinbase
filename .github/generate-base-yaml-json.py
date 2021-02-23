d1913b4caeef7de59265f54bfb3b226961aca9eb
Juuso Autiosalo <57953088+juusoautiosalo@users.noreply.github.com>
2021-02-23 12:16:41 +0200

    Add setup info to base yaml, enhance commenting

--- .github/generate-base-yaml-json.py
+++ .github/generate-base-yaml-json.py
@@ -2,30 +2,40 @@ import os
 import yaml
 import json
 
+# Get current working directory
 curdir = os.getcwd()
 
+# Initialize dictionary
 d = {}
 
+# Fetch GitHub repository info
 repo = os.environ["GITHUB_REPOSITORY"] # for local dev, set in bash: export GITHUB_REPOSITORY=dev-user/twinbase-dev
-
-owner = repo.split('/')[0]
-print('Owner: ' + owner)
-
+github_owner = repo.split('/')[0]
 repo_plain = repo.split('/')[1]
-print('Repo_plain: ' + repo_plain)
 
+# Fetch hosting URL
+# (Does not take into account user domain set in [user].github.io repo)
 try:
     with open('CNAME') as f:
         lines = f.readlines()
     baseurl = 'https://' + lines[0]
     d['baseurl'] = baseurl
 except:
-    d['baseurl'] = 'https://' + owner + '.github.io/' + repo_plain
+    d['baseurl'] = 'https://' + github_owner + '.github.io/' + repo_plain
 
+# Fetch owner info from setup.yaml file in repo root and add to dict
+with open('../setup.yaml', 'r') as yamlfile:
+    setup = yaml.load(yamlfile, Loader=yaml.FullLoader)
+d.update(setup)
+# Print owner DTID, this is also a test to make sure it exists
+print('Owner DTID: ' + setup['owner']['dt-id'])
+
+# Add repo specific URLs
 d['repo_url'] = 'https://github.com/' + repo
+d['new_twin_url'] = "https://github.com/" + repo \
+    + "/new/main?filename=docs/edit_this_with_backspace/index.yaml&value=Paste%20a%20DT%20document%20template%20here"
 
-d['new_twin_url'] = "https://github.com/" + repo + "/new/main?filename=docs/edit_this_with_backspace/index.yaml&value=Paste%20a%20DT%20document%20template%20here"
-
+# Add twins from twin folders
 print('Adding twins from folders:')
 d['twins'] = []
 i = 0
@@ -40,8 +50,10 @@ for folder in os.listdir(curdir):
         d['twins'][i]['name'] = dtdoc['name']
         i += 1
 
+# Write YAML file
 with open ('index.yaml', 'w') as yamlfile:
-    yaml.dump(d, yamlfile)
+    yaml.dump(d, yamlfile, default_flow_style=False, sort_keys=False, allow_unicode=True)
 
+# Write JSON file
 with open ('index.json', 'w') as jsonfile:
     json.dump(d, jsonfile)
